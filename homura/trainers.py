bae693a7a33775bb7d645f1ed7b12f50afa9d063
nick1392 <niccolo.bisagno@unitn.it>
2019-05-28 16:50:49 -0400

    print25

--- homura/trainers.py
+++ homura/trainers.py
@@ -355,9 +355,9 @@ class SupervisedTrainer(TrainerBase):
             self.iteration_id += 1
             print(self.iteration_id)
             
-            filename = 'outfile' + str(self.iteration_id)
+            filename = 'img/outfile' + str(self.iteration_id)
             np.save(filename, output.data.cpu().numpy())
-            filename_target = 'outfile_target' + str(self.iteration_id)
+            filename_target = 'img/outfile_target' + str(self.iteration_id)
             np.save(filename_target, target.data.cpu().numpy())
             
 #            with open('class'+ +'outfile' + str(self.iteration_id) + '.txt','w+') as f:
