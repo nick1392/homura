c2e9027d02b29795c7eece2b000fa45b9cede8fe
nick1392 <niccolo.bisagno@unitn.it>
2019-06-06 14:22:28 -0400

    print44

--- homura/trainers.py
+++ homura/trainers.py
@@ -366,9 +366,9 @@ class SupervisedTrainer(TrainerBase):
             self.iteration_id += 1
             #print(self.iteration_id)
             
-            filename = 'file/FC/epoch'+ str(self.epoch).zfill(3) +'FC' + str(self.iteration_id).zfill(4)
-            print(output)
-            np.save(filename, output.data.cpu().numpy())
+            #filename = 'file/FC/epoch'+ str(self.epoch).zfill(3) +'FC' + #str(self.iteration_id).zfill(4)
+            #print(output)
+            #np.save(filename, output.data.cpu().numpy())
             filename_target = 'file/target/epoch'+ str(self.epoch).zfill(3) +'target' + str(self.iteration_id).zfill(4)
             np.save(filename_target, target.data.cpu().numpy())
             
